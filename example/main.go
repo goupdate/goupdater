8adae02e64171a2873cebf5e7d2c6f16f090b5ea
ed <webdimmy@gmail.com>
2023-10-06 03:58:04 +0300

    example app

---
+++ example/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+)
+
+func main() {
+	ea := flag.Bool("ea", false, "enable updates?")
+	upload := flag.Bool("upload", false, "upload this?")
+
+	flag.Parse()
+
+	exitnow := make(chan bool)
+
+	if *ea {
+		Update(*upload)
+		if *upload {
+			fmt.Println("uploaded to server")
+			return
+		}
+	}
+
+	ticker := time.NewTicker(time.Second)
+
+	//do some stuff,
+	for {
+		select {
+		case <-ticker.C:
+			fmt.Println("im working...")
+
+		case <-exitnow:
+			fmt.Println("i was updated, restart")
+		}
+	}
+}
---
+++ example/update.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/goupdate/goupdater"
+)
+
+var BRANCH = "main"
+var PROJECT = "some-project"
+
+func Update(uploadthis bool) {
+	var update = goupdater.New("127.0.0.1:1980",
+		"key to upload and download",
+		PROJECT,
+		BRANCH)
+
+	update.Verbose(true)
+
+	if uploadthis {
+		err := update.Upload(BRANCH)
+		if err != nil {
+			fmt.Printf("error: %s\n", err.Error())
+		} else {
+			fmt.Println("Upload success!")
+		}
+		return
+	}
+
+	go func() {
+		for {
+			// check if there any new version of this file available?
+			ok, err := update.Check()
+			if err != nil {
+				fmt.Printf("update check: %s", err.Error())
+				return
+			}
+
+			if ok {
+				fmt.Println("new version found! download and upgrade!")
+				err := update.DownloadAndReplaceMe()
+				if err == nil {
+					fmt.Println("i was upgraded")
+					return
+				} else {
+					fmt.Printf("error during upgrade: %s\n", err.Error())
+				}
+			} else {
+				fmt.Println("no new version found")
+			}
+			time.Sleep(time.Minute)
+		}
+	}()
+}
